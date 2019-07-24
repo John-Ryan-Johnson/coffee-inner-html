42a182715fb3cb334eb9b9b99503be395855bab9
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-07-24 15:02:49 -0500

    initial commit

---
+++ coffee.js
@@ -0,0 +1,23 @@
+const bean = "Colombian"
+const style = "Espresso"
+const size = "Medium"
+const customer = {
+    firstName: "Johann",
+    lastName: "Abramovitz"
+ 
+}
+
+/* Use a string template that uses interpolation to generate the following string
+"Medium Colombian Espresso is ready for Johann Abramovitz!!" */
+
+const coffeeSentence = `"${size} ${bean} ${style} is ready for ${customer.firstName} ${customer.lastName} !!"`
+
+console.log(coffeeSentence)
+
+const completedOrders = document.getElementById("completedOrders") 
+
+/*
+1. Create a `<div>` element in HTML and give it an id attribute value of `completedOrders`
+2. Update the `innerHTML` property of `div` with the message that you created above. */
+
+completedOrders.innerHTML += coffeeSentence
